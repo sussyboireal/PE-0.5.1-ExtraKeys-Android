2b69fa1801b2290be65267758c558d7ce74977de
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-07 19:27:16 +0200

    For now

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -1292,11 +1292,15 @@ class CharacterEditorState extends MusicBeatState
 
 		if (data.length > 0)
 		{
+                        #if android
+                        SUtil.saveContent(daAnim, ".json", data);
+                        #else
 			_file = new FileReference();
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
 			_file.save(data, daAnim + ".json");
+                        #end
 		}
 	}
 
