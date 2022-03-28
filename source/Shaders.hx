444b6e561a6cc3c95f0ad8eacddb5480bd96b9a9
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-28 14:15:24 +0300

    Update Shaders.hx

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -1,7 +1,7 @@
 package;
 
 // STOLEN FROM HAXEFLIXEL DEMO LOL
-import flixel.system.FlxAssets.FlxShader;
+//import flixel.system.FlxAssets.FlxShader;
 import openfl.display.BitmapData;
 import openfl.display.Shader;
 import openfl.display.ShaderInput;
